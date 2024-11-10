bce107126b2a0f6c5b61d91b950e82aa9b3cac92
Anna Schwarz <Aousheh.y23@gmail.com>
2024-11-10 13:29:44 +0330

    add entites properties

--- src/main/java/entities/Tweet.java
+++ src/main/java/entities/Tweet.java
@@ -1,6 +1,5 @@
 package entities;
 
-import entities.enums.ArticleStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,29 +12,14 @@ import java.util.List;
 public class Tweet {
     private long id;
     private User user;
-    private String title;
-    private entities.Category category;
     private String content;
     private Date createDate;
-    boolean isPublished;
-    private Date lastUpdateDate;
-    private Date publishDate;
-    private ArticleStatus status;
-    private List<Tag> brief ;
+    private List<Tag> brief;
+    private List<User> likes;
+    private List<User> dislikes;
+    private List<User> views;
+    private List<Tweet> retweets;
 
-    public Tweet(long id, String title, String content, Category category,
-                 Date createDate, boolean isPublished,
-                 Date lastUpdateDate, ArticleStatus status , User user) {
-        this.title = title;
-        this.category = category;
-        this.content = content;
-        this.createDate = createDate;
-        this.isPublished = isPublished;
-        this.lastUpdateDate = lastUpdateDate;
-        this.status = status;
-        this.user = user;
-        this.id = id;
-    }
 
     public Tweet(User user, String title, Category category, String content) {
         this.user = user;
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,6 +1,5 @@
 package entities;
 
-import entities.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,30 +8,13 @@ import java.util.Date;
 @Getter
 @Setter
 public class User {
-    String username;
-    String password;
-    Role role;
-    long id;
-
-    public User(String username, String password, Role role) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
+    private String username;
+    private String password;
     private long id;
-    private final String firstName;
-    private final String lastName;
-    private final String nationalCode;
-    private final Date birthDate;
+    private String displayName;
+    private String email;
+    private String bio;
+    private final Date creationDate;
 
-
-    public User(String firstName, String lastName, String username, String password,
-                String nationalCode, Date birthday) {
-        super(username, password, Role.AUTHOR);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.nationalCode = nationalCode;
-        this.birthDate = birthday;
-    }
 
 }
